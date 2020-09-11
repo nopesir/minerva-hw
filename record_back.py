975945787ec0769e38d021f5f9b656bc0cf387e4
Luigi Ferrettino <s254300@studenti.polito.it>
2020-09-11 15:33:54 +0100

    updated to switch mode

--- record_back.py
+++ record_back.py
@@ -6,22 +6,49 @@ import ffmpeg
 import gpsd
 import time
 import os
+try:
+    import httplib
+except:
+    import http.client as httplib
+
+
 
 # Connect to the local gpsd
 gpsd.connect()
+os.chdir("/home/pi/records")
+def ping(host):
+    """
+    Returns True if host (str) responds to a ping request.
+    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+    """
 
+    # Option for the number of packets as a function of
+    param = '-c'
+
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, '1', host]
+
+    return subprocess.call(command) == 0
+
+
+def sync():
+    while True:
+        subprocess.call('rsync -vr --remove-source-files /home/pi/records/ pi@192.168.1.116:/home/pi/records', shell=True)
+        time.sleep(2)
+        if not ping("192.168.1.116"):
+            return
 
 
 # Define a function for the thread
 def gps(stamp):
     text_file = open(stamp + "/gps.txt", "w")
-    time.sleep(1)
+    time.sleep(3)
     i = 0
-    while i<151:
+    while i<300:
         i += 1
         packet = gpsd.get_current()
         n = text_file.write(str(i) + ' ' + str(packet.lat) + ' ' + str(packet.lon) + ' ' + str(packet.hspeed) + '\n')
-        time.sleep(0.0666667)
+        time.sleep(0.06667)
     text_file.close()
    #subprocess.call('sudo ffmpeg -y -framerate 15 -i ' + start + ' -r 15 -b:v 5000000 -c:v copy -f mp4 ' + end, shell=True)
    #time.sleep(0.5)
@@ -36,14 +63,17 @@ def gps(stamp):
 #camera.wait_recording(10)
 
 threads = []
-for i in range(2, 5):
+while True:
     #camera.split_recording('%d.h264' % ti)
-    stamp = str(int(time.time()))
+    stamp = str(int(time.time())+3)
     t = Thread(target=gps, args=(stamp, ))
     os.mkdir(stamp)
     t.start()
-    ffmpeg.input("/dev/video2", t=10, framerate=15, input_format="h264").output(stamp + '/video.mp4',t=10, framerate=15, bitrate=5000000, codec="copy").run()
+    ffmpeg.input("/dev/video2", t=20, framerate=15, input_format="h264").output(stamp + '/video.mp4',t=20, framerate=15, bitrate=5000000, codec="copy").run()
     t.join()
+    if ping("192.168.1.116"):
+        sync()
+
     #camera.wait_recording(10)
 
 #camera.stop_recording()
