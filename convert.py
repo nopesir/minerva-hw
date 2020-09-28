bff3048d2879066f13ef42fccdd31c5e7ed05b22
Luigi Ferrettino <s254300@studenti.polito.it>
2020-09-28 20:11:05 +0200

    added script gps map

---
+++ convert.py
@@ -0,0 +1,16 @@
+# Using readlines() 
+file1 = open('gps.txt', 'r') 
+Lines = file1.readlines() 
+  
+count = 0
+file2 = open('gps-out.txt', 'w') 
+file2.write('name,desc,latitude,longitude\n')
+# Strips the newline character 
+for line in Lines:
+    elems = line.strip().split()
+    print(elems)
+    if len(elems)>1:
+        file2.write(elems[0]+',' + elems[3]+',' + elems[1]+',' + elems[2]+'\n')
+
+file1.close()
+file2.close()
